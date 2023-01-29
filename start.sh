70678053993894f0e8284a9afaf31cc85d4690f3
prateeknana1 <123851001+prateeknana1@users.noreply.github.com>
2023-01-29 13:57:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /app
-echo "----- Now deployed web booting your repo ------ " 
-gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app
+git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
+$START_CMD
